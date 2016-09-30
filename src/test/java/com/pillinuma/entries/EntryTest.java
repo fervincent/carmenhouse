06b6c3111c73d234074a8682c4785310d47d2b0b
Fernando Vicente Abad Guamán <fervincent@gmail.com>
2016-09-29 20:35:59 -0500

    added basic archives

---
+++ src/test/java/com/pillinuma/entries/EntryTest.java
@@ -0,0 +1,16 @@
+package com.pillinuma.entries;
+
+import org.junit.Test;
+
+/**
+ * Created by fernando on 9/29/16.
+ */
+public class EntryTest {
+
+    @Test
+    public void testItShouldHaveProperties() {
+
+        System.out.println("hola");
+    }
+
+}
